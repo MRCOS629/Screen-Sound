539625a9ea447f0c867dfa8e350a0c2313558c72
MRCOS629 <matheusrochasouza2013@gmail.com>
2026-05-24 21:28:26 -0400

    Adicionando o programa
    Colocando o código do Screen Sound no ar

---
+++ Screen Sound/Screen Sound/Musica.cs
@@ -0,0 +1,36 @@
+﻿class Musica
+{
+
+    public Musica(Banda artista, string nome)
+    {
+        Artista = artista;
+        Nome = nome;
+    }
+
+    public Banda Artista { get; }
+    public string Nome { get; }
+    public int Duração { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida
+    {
+        get
+        {
+            string textoResumo = $"A música {Nome} é da banda {Artista}";
+            return textoResumo;
+        }
+    }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"\nNome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"duração: {Duração}");
+        if (Disponivel)
+        {
+            Console.WriteLine("A música está disponível no plano");
+        } else
+        {
+            Console.WriteLine("Assine o plano plus para ver essa música!");
+        }
+    }
+}
---
+++ Screen Sound/Screen Sound/Program.cs
@@ -0,0 +1,31 @@
+﻿Banda queen = new Banda("Queen");
+
+Album albumDoQueen = new Album("A night at the opera");
+
+Musica musica1 = new Musica(queen, "Love of my life")
+{
+    Duração = 213,
+    Disponivel = true,
+};
+
+
+Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
+{
+    Duração = 367,
+    Disponivel = false,
+};
+
+albumDoQueen.AdicionarMusicas(musica1);
+albumDoQueen.AdicionarMusicas(musica2);
+
+queen.AdicionarAlbum(albumDoQueen);
+queen.ExibirDiscografia();
+// 1. Exibir a ficha técnica de uma música específica
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+
+Console.WriteLine("\n" + new string('-', 30) + "\n");
+
+ //2. Exibir a lista completa do álbum
+albumDoQueen.ExibirMusicaDoAlbum();
+
